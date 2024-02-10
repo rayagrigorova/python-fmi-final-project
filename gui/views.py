ad7f2d8d4d4d9e90dbf47c4ec00ed24d4b837916
Raya <raiagrigorova@gmail.com>
2024-02-10 19:08:27 +0200

    Change html templates

--- gui/views.py
+++ gui/views.py
@@ -1,18 +1,15 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import UserRegistrationForm
 from django.shortcuts import render, redirect
+from .models import RegistrationCode
 
 
 @login_required(login_url='/register-login')
 def index(request):
     """Welcome page."""
     return render(request, 'index.html')
-
-
-from .models import RegistrationCode
 
 
 def register_and_login(request):
@@ -31,7 +28,7 @@ def register_and_login(request):
                 code_entry = RegistrationCode.objects.get(username=username, code=registration_code, is_activated=False)
                 valid_code = True
             except RegistrationCode.DoesNotExist:
-                messages.error(request, "Invalid registration code for this username or code already activated.")
+                pass
 
             if reg_form.is_valid() and valid_code:
                 user = reg_form.save(commit=False)
@@ -40,13 +37,13 @@ def register_and_login(request):
                 code_entry.is_activated = True
                 code_entry.save()
                 login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-                return redirect('login')
+                return redirect('/login/')
         elif 'action' in request.POST and request.POST['action'] == 'login':
             if login_form.is_valid():
                 user = authenticate(username=login_form.cleaned_data['username'],
                                     password=login_form.cleaned_data['password'])
                 if user is not None:
                     login(request, user)
-                    return redirect('index')
+                    return redirect('/index/')
 
     return render(request, 'registration/register_and_login.html', {'reg_form': reg_form, 'login_form': login_form})
