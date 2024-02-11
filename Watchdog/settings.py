97c530cab8e3741ba9ed3b86e132a99c9db4ef1d
Raya <raiagrigorova@gmail.com>
2024-02-11 10:30:42 +0200

    Format dog adoption posts

--- Watchdog/settings.py
+++ Watchdog/settings.py
@@ -133,5 +133,6 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/register-login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
