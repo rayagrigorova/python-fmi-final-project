d7cdc939cdc776e6a2eb3b106d6b3f1044362b3d
Raya <raiagrigorova@gmail.com>
2024-02-12 11:44:21 +0200

    Add login and registration tests

--- gui/tests.py
+++ gui/tests.py
@@ -7,6 +7,14 @@ from django.contrib.auth import get_user_model
 
 
 class UserRegistrationAndLoginTests(TestCase):
+    def setUp(self):
+        # This is a code that can be used when a new shelter user is registered
+        self.valid_code = RegistrationCode.objects.create(code="validcode123", username="shelteruser", is_activated=False)
+        # This is a code which is already activated (meaning that it's invalid and can't be used)
+        self.activated_code = RegistrationCode.objects.create(code="activatedcode123", username="shelteruser2", is_activated=True)
+        # An existing user is created to test if attempts to create a new user fail
+        CustomUser.objects.create_user(username='existinguser', password='testpassword123', role='ordinary')
+
     def test_register_and_login_ordinary_user(self):
         url = reverse('register_and_login')
         register_response = self.client.post(url, {
@@ -25,26 +33,63 @@ class UserRegistrationAndLoginTests(TestCase):
             'action': 'login'
         }, follow=True)
 
-        if login_response.context.get('login_form'):
-            print(login_response.context['login_form'].errors)
-
         self.assertTrue(login_response.context['user'].is_authenticated, "User login failed")
 
-    def test_register_and_login_shelter_user(self):
-        registration_code = RegistrationCode.objects.create(code='sheltercode123', username='shelteruser',
-                                                            is_activated=False)
-        print(RegistrationCode.objects.all().values())
-        url = reverse('register_and_login')
-        response = self.client.post(url, {
+    def test_shelter_registration_with_nonexistent_code(self):
+        response = self.client.post(reverse('register_and_login'), {
             'username': 'shelteruser',
             'password': 'testpassword123',
             'role': 'shelter',
-            'registration_code': 'sheltercode123',
+            'registration_code': 'nonexistent',
+            'action': 'register'
+        })
+        reg_form = response.context.get('reg_form')
+
+        self.assertFalse(reg_form.is_valid())
+        self.assertIn('registration_code', reg_form.errors)
+        self.assertEqual(reg_form.errors['registration_code'],
+                         ["Invalid registration code for this username or code already activated."])
+
+    def test_shelter_registration_with_activated_code(self):
+        response = self.client.post(reverse('register_and_login'), {
+            'username': 'shelteruser2',
+            'password': 'testpassword123',
+            'role': 'shelter',
+            'registration_code': self.activated_code.code,
+            'action': 'register'
+        })
+        reg_form = response.context.get('reg_form')
+
+        self.assertFalse(reg_form.is_valid())
+        self.assertIn('registration_code', reg_form.errors)
+        self.assertEqual(reg_form.errors['registration_code'],
+                         ["Invalid registration code for this username or code already activated."])
+
+    def test_shelter_registration_without_code(self):
+        response = self.client.post(reverse('register_and_login'), {
+            'username': 'shelteruser3',
+            'password': 'testpassword123',
+            'role': 'shelter',
+            'action': 'register'
+        })
+        reg_form = response.context.get('reg_form')
+
+        self.assertFalse(reg_form.is_valid())
+        self.assertIn('registration_code', reg_form.errors)
+        self.assertIn("This field is required for shelters.", reg_form.errors['registration_code'])
+
+    def test_register_and_login_shelter_user(self):
+        url = reverse('register_and_login')
+        self.client.post(url, {
+            'username': 'shelteruser',
+            'password': 'testpassword123',
+            'role': 'shelter',
+            'registration_code': 'validcode123',
             'action': 'register'
         })
 
         user_exists = get_user_model().objects.filter(username='shelteruser').exists()
-        code_activated = RegistrationCode.objects.get(code='sheltercode123').is_activated
+        code_activated = RegistrationCode.objects.get(code='validcode123').is_activated
         self.assertTrue(user_exists)
         self.assertTrue(code_activated)
 
